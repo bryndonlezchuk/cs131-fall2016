173f7d7bd120395b0c6d8f2c7bb2b77bc7cc1997
Bryndon Lezchuk <bryndonlezchuk@gmail.com>
2016-09-27 16:52:49 -0700

    Added header blocks to source code. Added project documentation

--- Project1/Project1/Project1.cpp
+++ Project1/Project1/Project1.cpp
@@ -1,8 +1,48 @@
+//---------------------------------------------------------
+// File: squares.c
+//
+// Functions:
+//    main()
+//---------------------------------------------------------
+
 #include <stdio.h>
 #include <stdlib.h>
 
-
+//---------------------------------------------------------
+// Function: main() 
+//
+// Title: Project1
+//
+// Description:
+//
+//
+// Programmer: Bryndon Lezchuk (bryndonlezchuk@gmail.com)
+//
+// Date: 9/27/16
+//
+// Version: 1.00
+//
+// Environment:
+//   Intel Core i5
+//   Software: OS: Windows 10 
+//   Compiles under Microsoft Visual Studio 2015
+// 
+// Input: 
+//
+// Output: 
+//
+// Parameters: void
+//
+// Returns: EXIT_SUCCESS for successful completion 
+//
+// History Log:
+//   9/27/16	bcl		started version 1.0
+//---------------------------------------------------------
 int main(void)
 {
 
+
+
+
+	return EXIT_SUCCESS;
 }
