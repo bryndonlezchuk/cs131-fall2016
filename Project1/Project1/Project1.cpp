017a291d71cf290009b6cba563bbe92d3ce05aed
Bryndon Lezchuk <bryndonlezchuk@gmail.com>
2016-09-27 17:24:35 -0700

    Began building main() function

--- Project1/Project1/Project1.cpp
+++ Project1/Project1/Project1.cpp
@@ -1,5 +1,5 @@
 //---------------------------------------------------------
-// File: squares.c
+// File: Project1.cpp
 //
 // Functions:
 //    main()
@@ -13,7 +13,7 @@
 //
 // Title: Project1
 //
-// Description:
+// Description: Change Counter
 //
 //
 // Programmer: Bryndon Lezchuk (bryndonlezchuk@gmail.com)
@@ -28,8 +28,11 @@
 //   Compiles under Microsoft Visual Studio 2015
 // 
 // Input: 
+//		- a dollar amount for a purchase
+//		- the amount tendered for the purchase
 //
 // Output: 
+//		- a listing of the change
 //
 // Parameters: void
 //
@@ -40,9 +43,42 @@
 //---------------------------------------------------------
 int main(void)
 {
+	/*
+		Design, write and test a fully documented, user 
+		friendly ANSI C program which takes in (a) a dollar 
+		amount for a purchase and (b) the amount tendered
+		for the purchase. The program should then give a 
+		listing of the change, specifically in the 
+		denominations listed below.
 
+		To do this, calculate the total change as an 
+		integer number of cents. It should be accurate 
+		for a positive, zero or negative amount of change. 
+		Enter in the cost and tendered amounts as fixed 
+		floating point numbers, rounding off to the nearest 
+		cent--be careful not to lose pennies due to rounding 
+		errors. Print these values to two places. Calculate 
+		the total change as a floating point number, round 
+		off to the nearest cent, and display it to two 
+		decimal places. Now use an integer variable to store 
+		this number (multiplied by 100) as the number of 
+		cents. Divide to find the number of twenties, tens, 
+		etc., exactly as in the example below--no other 
+		denominations. You MUST use the % (or %=) operator 
+		to find the remainder after each denomination is 
+		removed from the total. Do not use arrays or loops.
+	*/
 
+	printf("Welcome to Change Counter by Bryndon Lezchuk!\n\n");
+	printf("\tPlease enter the total amount of purchase: $");
+	//input total amount of purchase
+	printf("\t"); //output amount entered rounded to nearest cent
 
+	printf("\tPlease enter amount of money tendered: $");
+	//input tender amount
+	printf("\t"); //output amount entered rounded to nearest cent
+
+	
 
 	return EXIT_SUCCESS;
 }
