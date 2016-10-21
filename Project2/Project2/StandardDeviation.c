9be94447d30a7d6e6fce624cdb761434fdc9eee0
Bryndon Lezchuk <bryndonlezchuk@gmail.com>
2016-10-20 17:19:53 -0700

    started handling reading in data from file

--- Project2/Project2/StandardDeviation.c
+++ Project2/Project2/StandardDeviation.c
@@ -7,6 +7,8 @@
 #include <string.h>
 #include <conio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
 #pragma warning(disable: 4996) 
 //----------------------------------------------------------------------------
 // Sample Code showing command line parameters and file handles
@@ -14,6 +16,18 @@
 //----------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
+	int firstPass = 1; // 1=true; 0=false
+	int numElements = 0;
+	int currentElement = 0;
+	int totalNumElements = 0;
+	int sumOfElements = 0;
+	int lowElement = 0;
+	int highElement = 0;
+	float meanOfElements = 0.0f;
+	float varianceOfElements = 0.0f;
+	float deviationOfElements = 0.0f;
+
+
 	FILE * inFileHandle = NULL;
 	FILE * outFileHandle = NULL;
 	char filename[FILENAME_MAX] = "";
@@ -71,12 +85,37 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-			while ((c = (char)getc(inFileHandle)) != EOF)
+			//while ((c = (char)getc(inFileHandle)) != EOF)
+			//{
+			//	//if (c == ' ')
+			//	//	c = '\n';
+			//	//putc(c, outFileHandle);
+			//}
+
+			while ((fscanf(inFileHandle, "%d", &numElements)) == 1)
 			{
-				if (c == ' ')
-					c = '\n';
-				putc(c, outFileHandle);
+				for (int i = 0; i < numElements; i++)
+				{
+					totalNumElements++;
+					fscanf(inFileHandle, "%d", &currentElement);
+
+					if (firstPass)
+					{
+						lowElement, highElement = currentElement;
+					}
+					else if (currentElement < lowElement)
+					{
+						lowElement = currentElement;
+					}
+					else if (currentElement > highElement)
+					{
+						highElement = currentElement;
+					}
+
+
+				}
 			}
+
 			fclose(inFileHandle);
 			fclose(outFileHandle);
 			puts("\nPress any key to Continue");
