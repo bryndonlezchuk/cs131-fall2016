a255cb1ebe819ca5157b0f107a79e13ab4fdf222
Bryndon Lezchuk <90littlegreenmen@gmail.com>
2016-10-26 20:51:28 -0700

    finished up to minimum standards

--- Project2/Project2/StandardDeviation.c
+++ Project2/Project2/StandardDeviation.c
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------
-// File: argcv.c
+// File: StandardDeviation.c
 // Functions:
 //		int main(int argc, char *argv[])
 //----------------------------------------------------------------------------
@@ -11,8 +11,48 @@
 #include <math.h>
 #pragma warning(disable: 4996) 
 //----------------------------------------------------------------------------
-// Sample Code showing command line parameters and file handles
-// CS& 131
+// Function:	main() 
+//
+// Title:		Project2
+//
+// Description:	Standard Deviation
+//
+//
+// Programmer:	Bryndon Lezchuk (bryndonlezchuk@gmail.com)
+//
+// Date:		9/27/16
+//
+// Version:		1.00
+//
+// Environment:	Intel Core i5
+//				Software: OS: Windows 10 
+//				Compiles under Microsoft Visual Studio 2015
+//
+//				Intel Core i7
+//				Software: OS: Windows 7
+//				Compiles under Microsoft Visual Studio 2015 Community
+// 
+// Input:		- user inputs the filename to read data from
+//				- user inputs the filename to write output to
+//
+// Output:		To a file:
+//				- number of data
+//				- sum of data
+//				- range of data
+//				- mean of data
+//				- variance
+//				- standard deviation
+//
+// Calls:		
+//
+// Parameters:	int argc, char *argv[]
+//
+// Returns:		EXIT_SUCCESS for successful completion 
+//				EXIT_FAILURE for failed completion
+//
+// History Log:
+//				10/11/16	bcl		started version 1.0
+//				10/23/16	bcl		finished version 1.0
 //----------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
@@ -21,11 +61,12 @@ int main(int argc, char *argv[])
 	int currentElement = 0;
 	int totalNumElements = 0;
 	int sumOfElements = 0;
+	int sumOfElementsSquares = 0;
 	int lowElement = 0;
 	int highElement = 0;
 	float meanOfElements = 0.0f;
 	float varianceOfElements = 0.0f;
-	float deviationOfElements = 0.0f;
+	double deviationOfElements = 0.0f;
 
 
 	FILE * inFileHandle = NULL;
@@ -100,35 +141,46 @@ int main(int argc, char *argv[])
 
 			while ((fscanf(inFileHandle, "%d", &numElements)) == 1)
 			{
-				// for each element
-				for (int i = 0; i < numElements; i++)
+				if (numElements != 0)
 				{
-					totalNumElements++;
-					fscanf(inFileHandle, "%d", &currentElement);
+					// for each element
+					for (int i = 0; i < numElements; i++)
+					{
+						totalNumElements++;
+						fscanf(inFileHandle, "%d", &currentElement);
 
-					if (firstPass)
-					{
-						lowElement = currentElement;
-						highElement = currentElement;
-						firstPass = 0;
-					}
-					else if (currentElement < lowElement)
-					{
-						lowElement = currentElement;
-					}
-					else if (currentElement > highElement)
-					{
-						highElement = currentElement;
+						if (firstPass)
+						{
+							lowElement = currentElement;
+							highElement = currentElement;
+							firstPass = 0;
+						}
+						else if (currentElement < lowElement)
+						{
+							lowElement = currentElement;
+						}
+						else if (currentElement > highElement)
+						{
+							highElement = currentElement;
+						}
+
+						sumOfElements += currentElement;
+						sumOfElementsSquares += (currentElement * currentElement);
 					}
 
-					sumOfElements += currentElement;
+					// for each group of elements
+					meanOfElements = (float)sumOfElements / totalNumElements;
+
+					if (totalNumElements > 1)
+					{
+						varianceOfElements = (sumOfElementsSquares - ((float)(sumOfElements * sumOfElements) / totalNumElements)) / (totalNumElements - 1);
+					}
+					deviationOfElements = sqrt(fabs(varianceOfElements));
 				}
 
-				// for each group of elements
-				meanOfElements = sumOfElements / totalNumElements;
-
-
-				fprintf(outFileHandle, "|%5d|%5d|%8d|%5d|%8d|%5d|\n");
+				fprintf(outFileHandle, "| %7d | %5d | %3d to %2d | %4.3lf| %8.3lf | %17.3lf |\n", totalNumElements, sumOfElements,
+					lowElement, highElement, meanOfElements, varianceOfElements, deviationOfElements);
+				fprintf(outFileHandle, "_____________________________________________________________________\n");
 			}
 
 			fclose(inFileHandle);
